0e2a2b0eb4909f0a43456873cd68541ba1e36878
Amilton Junior <amilton.j@uni9.edu.br>
2025-08-04 17:30:25 -0300

    Update agendamento.js

--- agendamento.js
+++ agendamento.js
@@ -7,8 +7,9 @@ document.getElementById('scheduling-form').addEventListener('submit', function (
   const time = document.getElementById('time').value;
 
   const message = `Olá! Gostaria de agendar um horário:\n\nNome: ${name}\nServiço: ${service}\nData: ${date}\nHorário: ${time}`;
-  const phone = '5511954246629'; // Substitua com o número do seu WhatsApp com DDD e código do país (55 para Brasil)
+  const phone = '********'; // Substitua com o número do seu WhatsApp com DDD e código do país (55 para Brasil)
 
   const url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
   window.open(url, '_blank');
 });
+
